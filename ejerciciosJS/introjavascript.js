26fcfa85d0ebb2bf11d6721ec2ba482eaa31dc14
gengarbone <75919423+gengarizzy@users.noreply.github.com>
2023-02-15 14:54:56 -0300

    primer commit. DESKTOP terminado con validador ok. Falta RESPONSIVE

---
+++ ejerciciosJS/introjavascript.js
@@ -0,0 +1,42 @@
+let miNombre;
+
+miNombre = "Nekinu";
+
+console.log(miNombre);
+
+let miApellido;
+miApellido = "Kujo";
+
+console.log(miApellido);
+
+let miEdad;
+miEdad = 27;
+console.log(miEdad);
+
+let miMascota;
+miMascota = "Estrella panales Estrella";
+console.log(miMascota);
+
+let edadMascota;
+edadMascota = 0.2;
+console.log(edadMascota);
+
+let nombreCompleto;
+nombreCompleto = miNombre  + " " + miApellido;
+console.log(nombreCompleto);
+
+let textoPresentacion;
+textoPresentacion = miNombre + " " + miApellido + " " + miEdad + " " + miMascota + " " + nombreCompleto;
+console.log(textoPresentacion);
+
+let sumaEdades;
+sumaEdades = miEdad + edadMascota;
+console.log("sumaEdades " + sumaEdades);
+
+let restaEdades;
+restaEdades = miEdad - edadMascota
+console.log("restaEdades " + restaEdades);
+
+let productoEdades;
+productoEdades = miEdad * edadMascota;
+console.log("productoEdades " + productoEdades);
