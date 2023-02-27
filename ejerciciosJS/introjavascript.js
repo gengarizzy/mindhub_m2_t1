96bcefe9aa31851ffc30bcc4bc2fdf5357d429e5
gengarbone <75919423+gengarizzy@users.noreply.github.com>
2023-02-27 14:20:39 -0300

    Primera resolucion. Me falta poder usar el array de events desde su archivo original

--- ejerciciosJS/introjavascript.js
+++ ejerciciosJS/introjavascript.js
@@ -21,6 +21,17 @@ let edadMascota;
 edadMascota = 0.2;
 console.log(edadMascota);
 
+
+
+
+// EJ 10
+
+// miNombre = prompt('Introduce tu mombre');
+// miApellido = prompt('Introduce tu apellido');
+// miEdad = prompt('Introduce tu edad');
+// edadMascota = prompt('Introduce la edad de tu mascota');
+
+
 let nombreCompleto;
 nombreCompleto = miNombre  + " " + miApellido;
 console.log(nombreCompleto);
@@ -37,6 +48,71 @@ let restaEdades;
 restaEdades = miEdad - edadMascota
 console.log("restaEdades " + restaEdades);
 
-let productoEdades;
-productoEdades = miEdad * edadMascota;
+
+
+let productoEdades = miEdad * edadMascota;
 console.log("productoEdades " + productoEdades);
+
+
+
+// SEGUNDA PARTE
+
+let alumno = {
+
+    nombre: "Daniel",
+    apellido: "Palacio",
+    carrera: "Ingenieria Ambiental",
+    sedePrincipal: "Campus Lynch",
+    profesorEnMasMaterias: "Irene Wais"
+
+
+
+}
+
+console.table(alumno);
+
+
+let mascota = {
+
+    nombre: "Estrella",
+    apellido: "Tercera",
+    sede: "Da Vinci",
+    apodo: "Smol",
+    ataqueEspecial: "Mearle las cosas a Nekinu"
+}
+
+console.table(mascota);
+
+let frutas = ["Banana", "Manzana", "Limon", "Naranja", "Mango"]
+console.table(frutas);
+// console.table(frutas[0]);
+// console.table(frutas[1]);
+console.table(frutas[2]);
+// console.table(frutas[3]);
+// console.table(frutas[4]);
+
+let numeros = ["0", "1" , "2", "3", "4"]
+console.table(numeros);
+
+let familia = ["Familiar 1", "Familiar 2","Familiar 3","Familiar 4","Familiar 5"]
+console.table(familia);
+
+textoAleatorio = frutas[1] + " " + numeros[3] + " " + frutas[4];
+console.log(textoAleatorio);
+
+let edadmia = prompt("Introduce tu edad");
+let edadcompa = prompt("introduce la edad de tu compa");
+
+edadesIguales = edadmia==edadcompa;
+soyMayor = edadmia>edadcompa;
+soyMenor = edadmia<edadcompa;
+
+console.log("Mi edad es igual a la de mi compa es: ",edadesIguales); 
+console.log("Soy mayor que mi compa: ",soyMayor);
+console.log("Soy menor que mi compa ",soyMenor);
+
+
+
+Edad = prompt("Introduci tu edad, a ver si soy mayor de edad");
+soyMayorDeEdad = Edad>=18;
+console.log("Sos mayor de edad: ",soyMayorDeEdad);
